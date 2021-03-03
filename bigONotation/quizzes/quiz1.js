6d3050f59b246cea876b59abd6e8a6399bd9f66a
techtwins <gshevaughn@gmail.com>
2021-03-03 13:36:17 -0500

    completed question 2

--- bigONotation/quizzes/quiz1.js
+++ bigONotation/quizzes/quiz1.js
@@ -1,9 +1,18 @@
 // Determine the time complexity for the following functions
 
-// Time complexity: O(n)
+// Question 1
 function logUpTo(n) {
   for (var i = 1; i <= n; i++) {
     console.log(i);
   }
 }
 logUpTo(10);
+// Answer: O(n)
+
+// Question 2
+function logAtMost10(n) {
+  for (var i = 1; i <= Math.min(n, 10); i++) {
+    console.log(i);
+  }
+}
+logAtMost10(20);
