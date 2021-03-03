60cc269b969502311db739db436853dcc0bf1643
techtwins <gshevaughn@gmail.com>
2021-03-03 14:27:05 -0500

    finished final question for space complexity

--- bigONotation/quizzes/quiz2.js
+++ bigONotation/quizzes/quiz2.js
@@ -34,3 +34,20 @@ function onlyElementsAtEvenIndex(array) {
 }
 console.log(onlyElementsAtEvenIndex([0, 1, 2, 3, 4]));
 // Answer: O(n)
+
+// ----------------- //
+
+// Question 4
+function subtotals(array) {
+  var subtotalArray = Array(array.length);
+  for (var i = 0; i < array.length; i++) {
+    var subtotal = 0;
+    for (var j = 0; j <= i; j++) {
+      subtotal += array[j];
+    }
+    subtotalArray[i] = subtotal;
+  }
+  return subtotalArray;
+}
+console.log(subtotals([1, 2, 3, 4]));
+// Answer: O(n)
