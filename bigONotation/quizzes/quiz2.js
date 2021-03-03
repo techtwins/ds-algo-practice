89667030f5438ecf12417e8e0bc984a4dd31cd01
techtwins <gshevaughn@gmail.com>
2021-03-03 14:15:30 -0500

    completed question 1 for space complexity

---
+++ bigONotation/quizzes/quiz2.js
@@ -0,0 +1,10 @@
+// Determine the space complexity for the following functions
+
+// Question 1
+function logUpTo(n) {
+  for (var i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+logUpTo(5);
+// Answer: O(1)
