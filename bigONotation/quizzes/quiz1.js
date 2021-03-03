713b9a9632fbaffc8b22ccc6368236f67e81fc63
techtwins <gshevaughn@gmail.com>
2021-03-03 13:55:52 -0500

    completed question 5

--- bigONotation/quizzes/quiz1.js
+++ bigONotation/quizzes/quiz1.js
@@ -45,3 +45,20 @@ function onlyElementsAtEvenIndex(array) {
 }
 console.log(onlyElementsAtEvenIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 // Answer: O(n)
+
+// ----------------- //
+
+// Question 5
+function subtotals(array) {
+  var subtotalArray = Array(array.length);
+  for (var i = 0; i < array.length; i++) {
+    var subtotal = 0;
+    for (var j = 0; j <= i; j++) {
+      subtotal += array[j];
+    }
+    subtotalArray[i] = subtotal;
+  }
+  return subtotalArray;
+}
+console.log(subtotals([1, 2, 3, 4, 5]));
+// Answer: O(n^2)
