4be0580dd5929e84b40ae1c7d4a63a0d907cdd98
techtwins <gshevaughn@gmail.com>
2021-03-03 13:33:40 -0500

    completed question 1 from the quiz

---
+++ bigONotation/quizzes/quiz1.js
@@ -0,0 +1,9 @@
+// Determine the time complexity for the following functions
+
+// Time complexity: O(n)
+function logUpTo(n) {
+  for (var i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+logUpTo(10);
