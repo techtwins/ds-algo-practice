ca6d96442f77d4ab3b05de1835ee463365b2c21e
techtwins <gshevaughn@gmail.com>
2021-03-03 13:47:27 -0500

    completed question 4

--- bigONotation/quizzes/quiz1.js
+++ bigONotation/quizzes/quiz1.js
@@ -30,3 +30,18 @@ function logAtLeast10(n) {
 }
 logAtLeast10(20);
 // Answer: O(n)
+
+// ----------------- //
+
+// Question 4
+function onlyElementsAtEvenIndex(array) {
+  var newArray = Array(Math.ceil(array.length / 2));
+  for (var i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      newArray[i / 2] = array[i];
+    }
+  }
+  return newArray;
+}
+console.log(onlyElementsAtEvenIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// Answer: O(n)
